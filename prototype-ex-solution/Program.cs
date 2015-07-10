7d7ca8bd5fdbb8e3ce0045d8eccfe11f35a641a0
dan <danv@cstructor.com>
2015-07-09 20:11:37 -0700

    add prototype-ex-solution

---
+++ prototype-ex-solution/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace prototype_ex_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myConcrete = new Concrete { Id = "1", Name = "one" };
+
+            var anotherConcrete = myConcrete.Clone();
+        }
+    }
+}
---
+++ prototype-ex-solution/Prototype.cs
@@ -0,0 +1,18 @@
+﻿namespace prototype_ex_solution
+{
+    public interface IPrototype<T>
+    {
+        T Clone();
+    }
+
+    public class Concrete : IPrototype<Concrete>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public Concrete Clone()
+        {
+            return MemberwiseClone() as Concrete;
+        }
+    }
+}
