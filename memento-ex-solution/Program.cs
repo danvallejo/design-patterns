e0000a790b19c6cffd445e6e858081924348b9d5
Vallejo, Dan <Dan.Vallejo@exchange.nordstrom.com>
2015-07-15 10:59:12 -0700

    add memento and solution

---
+++ memento-ex-solution/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace memento_ex_solution
+{
+    public class DataLogger
+    {
+        private const string FileName = "datalog.txt";
+
+        public DataLogger()
+        {
+            File.Delete(FileName);
+        }
+
+        public void Add(string line)
+        {
+            File.AppendAllText(FileName, line + "\n");
+        }
+
+        public Memento SaveToMemento()
+        {
+            return new Memento(FileName);
+        }
+
+        public void RestoreFromMemento(Memento memento)
+        {
+            File.Delete(FileName);
+            File.AppendAllLines(FileName, memento.State);
+        }
+    }
+
+    public class Memento
+    {
+        public Memento(string fileName)
+        {
+            State = File.ReadAllLines(fileName);
+        }
+
+        public string[] State
+        {
+            get;
+            private set;
+        }
+    }
+
+    public class Caretaker
+    {
+        private readonly List<Memento> mementos;
+
+        public Caretaker()
+        {
+            mementos = new List<Memento>();
+        }
+
+        public void Add(Memento memento)
+        {
+            mementos.Add(memento);
+        }
+
+        public Memento Get(int index)
+        {
+            return mementos[index];
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var originator = new DataLogger();
+
+            originator.Add("line 1");
+            originator.Add("line 2");
+
+            var caretaker = new Caretaker();
+            caretaker.Add(originator.SaveToMemento());
+
+            originator.Add("line 3");
+
+            originator.RestoreFromMemento(caretaker.Get(0));
+        }
+    }
+}
---
+++ memento/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace memento
+{
+    public class Originator
+    {
+        private List<string> users = new List<string>();
+
+        public void Add(string user)
+        {
+            users.Add(user);
+        }
+
+        public Memento SaveToMemento()
+        {
+            return new Memento(users);
+        }
+
+        public void RestoreFromMemento(Memento memento)
+        {
+            users = memento.State.ToList();
+        }
+    }
+
+    public class Memento
+    {
+        public Memento(IEnumerable<string> users)
+        {
+            State = users.ToList();
+        }
+
+        public IEnumerable<string> State
+        {
+            get;
+            private set;
+        }
+    }
+
+    public class Caretaker
+    {
+        private readonly List<Memento> mementos;
+
+        public Caretaker()
+        {
+            mementos = new List<Memento>();
+        }
+
+        public void Add(Memento memento)
+        {
+            mementos.Add(memento);
+        }
+
+        public Memento Get(int index)
+        {
+            return mementos[index];
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var originator = new Originator();
+
+            originator.Add("dave");
+            originator.Add("mike");
+
+            var caretaker = new Caretaker();
+            caretaker.Add(originator.SaveToMemento());
+
+            originator.Add("lisa");
+
+            originator.RestoreFromMemento(caretaker.Get(0));
+        }
+    }
+}
