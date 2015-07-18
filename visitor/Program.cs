60c1f27a75d5d16ada07cc16d9839356e44d5d21
Dan <danv@cstructor.com>
2015-07-18 16:52:54 -0700

    refactor visitor.cs

--- visitor/Program.cs
+++ visitor/Program.cs
@@ -1,41 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace visitor
 {
-   
-
-    /// <summary>
-    /// The 'ObjectStructure' class
-    /// </summary>
-    class Employees
-    {
-        private List<Employee> _employees = new List<Employee>();
-
-        public void Attach(Employee employee)
-        {
-            _employees.Add(employee);
-        }
-
-        public void Detach(Employee employee)
-        {
-            _employees.Remove(employee);
-        }
-
-        public void Accept(IVisitor visitor)
-        {
-            foreach (var e in _employees)
-            {
-                e.Accept(visitor);
-            }
-            Console.WriteLine();
-        }
-    }
-
-    // Three employee types
-
-   
-
     class Program
     {
         static void Main()
